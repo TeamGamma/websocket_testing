c87999388f6ae4f715696e9a3113642ea585fc71
robbles <odwyerrob@gmail.com>
2012-05-06 21:38:03 -0400

    Basic websocket and chat infrastructure

--- server.js
+++ server.js
@@ -1,39 +1,58 @@
-var WebSocket = require('faye-websocket'),
-    http      = require('http');
+var WebSocket = require('faye-websocket');
+var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 
-server.on('request', function(req, res) {
-  if(req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404 Not Found');
-  }
-});
-
+// Handle special WebSocket request
 server.on('upgrade', function(request, socket, head) {
+
+  // Initialize a new WebSocket connection using the HTTP connection
   var ws = new WebSocket(request, socket, head);
 
   ws.onopen = function(event) {
-    console.log('open');
+    ws.send('Welcome!');
+    console.log('WebSocket connection opened');
   };
 
   ws.onmessage = function(event) {
-    console.log('Received: "' + event.data + '"');
+    console.log('WebSocket received: "' + event.data + '"');
 
-    ws.send('timer started');
-    setTimeout(function() {
-      ws.send('timer finished');
+    // echo back to client in uppercase
+    ws.send(event.data.toUpperCase());
+
+    // start a 5s timer
+    ws.timeout = setTimeout(function() {
+      // send a message and close the connection
+      ws.send('I\'m disconnecting you, goodbye!');
       ws.close();
     }, 5000);
   };
 
   ws.onclose = function(event) {
-    console.log('close', event.code, event.reason);
-    ws = null;
+    console.log('WebSocket connection closed: ', event.code, event.reason);
+
+    // Cancel the timeout if it still exists
+    clearTimeout(ws.timeout);
   };
 });
 
+// Handle ordinary HTTP request
+server.on('request', function(request, response) {
+  if(request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+
+    // Serve index.html
+    fs.readFile('./index.html', 'utf-8', function (err, file) {
+      response.end(file, 'utf-8');
+    });
+
+  } else {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.end('404 Not Found');
+  }
+});
+
 server.listen(9001);
+console.log('Server running on http://localhost:9001');
+
